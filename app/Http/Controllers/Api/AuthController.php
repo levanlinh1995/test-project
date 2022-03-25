c71e246db36daea58943c9862f1e90c1ba4d3d5c
le linh <levanlinh1995@gmail.com>
2022-03-25 12:09:27 +0700

    fix bug: log out function

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -64,7 +64,7 @@ class AuthController extends Controller
         ]);
     }
 
-    public function logout()
+    public function logout(Request $request)
     {
         $request->user()->currentAccessToken()->delete();
 
